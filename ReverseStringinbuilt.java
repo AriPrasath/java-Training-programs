867b23e6cd63c699472d09f43f7aca07e1423e0c
AriPrasath <45748718+AriPrasath@users.noreply.github.com>
2018-12-10 14:49:58 +0530

    Update ReverseStringinbuilt.java
    Added Comment section

--- ReverseStringinbuilt.java
+++ ReverseStringinbuilt.java
@@ -2,19 +2,19 @@ package ari;
 
 public class ReverseStringinbuilt {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {           		 //main method
 		
 		
-		String str = "Welcome";
-		char ch = 'l';
-		String newString = str.substring(str.indexOf(ch));
+		String str = "Welcome";                      		//String
+		char ch = 'l';				     		//Char
+		String newString = str.substring(str.indexOf(ch));	//Creating String Subsrtring From the char to the end
 		
-		String result = str.substring(0,str.indexOf(ch));
+		String result = str.substring(0,str.indexOf(ch));	//Creating String from Starting to the character.
 		
-		int j=newString.length()-1;
-		char arr[] = newString.toCharArray();
+		int j=newString.length()-1;				//Finding length of String to be reversed
+		char arr[] = newString.toCharArray();			//Converting String to be reversed to char array
 		
-		for(int i=0;i<j;i++)
+		for(int i=0;i<j;i++)					//Loop to reverse a String By using index Starting from 0 to length of it.
 		{
 			
 				char temp=arr[i];
@@ -23,8 +23,8 @@ public class ReverseStringinbuilt {
 				j--;
 			
 		}
-		String s2=new String(arr);
-		System.out.println(newString);
+		String s2=new String(arr);				//Converting Chararray to String 
+		System.out.println(result+s2);				//Printing String as a whole
 	}
 
 }
